4d0c7721df41e90e2f145597c07786ba6b137464
Peter Quistgaard <peter_quistgaard@hotmail.com>
2018-08-19 14:35:08 +0200

    Init 2

--- SaveFileFromUrlWithWebclient/Controllers/HomeController.cs
+++ SaveFileFromUrlWithWebclient/Controllers/HomeController.cs
@@ -1,30 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.IO;
+using System.Net;
 using System.Web.Mvc;
 
 namespace SaveFileFromUrlWithWebclient.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string message)
+        {
+            ViewBag.message = message;
+            return View();
+        }
+
+        // GET: 
+        public ActionResult Create()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Create(string url)
         {
-            ViewBag.Message = "Your application description page.";
+            if (ModelState.IsValid) { 
+                try
+                {
+                    if (url == null || url.Length == 0)
+                    {
+                        ModelState.AddModelError("url", "No URI is Specifyed");                        
+                    }
 
+                    WebClient webClient = new WebClient();
+                    using (webClient)
+                    {
+                        string mimetype = GetMimeType(url);
+
+                        if (GetApprovedMimetypes().Contains(mimetype))
+                        {
+                            string path = GetApp_DataPath();
+                            string filename = GetFilename(url);
+                            webClient.DownloadFile(url, path + filename);
+                            return RedirectToAction("Index", new {message= String.Format("The file \"{0}\" is uploaded to App_Data", filename) });
+                        }
+                        else {
+                            ModelState.AddModelError("url", String.Format("Mimetype: {0} not allowed", mimetype));                            
+                        }
+                    }                    
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError("url", exception.Message);
+                }
+            }
             return View();
         }
-
-        public ActionResult Contact()
+        /// <summary>
+        /// is url on list of approved mime types?
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private bool isMimetypeApproved(string url)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            string mimetype = GetMimeType(url);
+            List<string> allowedMimetypes = GetApprovedMimetypes();
+            if (allowedMimetypes.Contains(mimetype)){
+                return true;
+            }
+            else {
+                return false;
+            }          
         }
+
+        /// <summary>
+        /// list of approved mime types
+        /// </summary>
+        /// <returns></returns>
+        private static List<string> GetApprovedMimetypes()
+        {
+            return new List<string>() { "application/pdf","image/jpeg", "image/png", "image/gif" };          
+        }
+
+
+        private string GetApp_DataPath()
+        {
+            return Server.MapPath("~/App_Data/");
+        }
+
+        private static string GetMimeType(string url)
+        {
+            string result = "";
+            WebRequest webRequest = WebRequest.Create(url);
+            WebResponse webResponse = webRequest.GetResponse();
+            result = webResponse.ContentType;
+            webResponse.Close();
+            return result;
+        }
+
+        private static string GetFilename(string url)
+        {
+            string result = "";
+            var uri = new Uri(url);
+            var fullName = uri.LocalPath.TrimStart('/');
+            result = Path.GetFileName(fullName);
+            return result;
+        }
+
     }
 }
